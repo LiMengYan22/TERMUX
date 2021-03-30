67a8476f0955868df2e3bd66f6168a48688797b0
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-30 13:57:56 +0200

    Add files via upload

---
+++ php/MENU.php
@@ -0,0 +1,252 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 🆃🅴🆁🅼🆄🆇 </title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+                <link href="images/termux.ico" rel="icon">
+                <link href="images/termux.ico" rel="apple-touch-icon">
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header"><h1><a href="index.php">🆃🅴🆁🅼🆄🆇 </a></h1>
+						<nav>
+							<ul>
+<li><a href="index.html" class="icon solid fa-chevron-circle-right">Volver</a></li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">		
+<!-- --------------------------ARTICULOS: -->
+<!-- ARTICULO 
+<article class="thumb">
+<a href="images/fulls/10.jpg" class="image"><img src="images/thumbs/10.jpg" alt="" /></a>
+<h2>TITULO</h2>
+<p>descripcion y nuemro</p>
+</article>-->
+<!-- //ARTICULO -->
+
+
+
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/10.jpg" class="image"><img src="images/thumbs/10.jpg" alt="" /></a>
+<h2>INDICE</h2>
+<!--<p>
+1.<br>
+2.<br>
+3.<br>
+4.<br>
+5.<br>
+6.<br>
+7.<br>
+8.<br>
+9.<br>
+</p>-->
+</article>
+<!-- //ARTICULO -->
+
+
+<!-- -------------------Ultimo ARTICULO -->
+
+
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/10.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112147433-00a86000-8bdd-11eb-8c28-f3548b16a482.jpg" alt="" /></a>
+<h2>Print Enviroment
+</h2>
+<p>Print Enviroment: Imprime las variables del entorno ANDROID en TERMUX.
+<br>
+<code>$ apt install printenv<br>
+$ pintenv</code>
+<br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/09.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112285463-a1a12480-8c8a-11eb-81fb-70ea8b2ea5ae.jpg" alt="" /></a>
+<h2>WEATHERFORECAST</h2>
+<p>WEATHERFORECAST (predicción del clima):<br>
+<code>$ mkdir weatherForecast<br>
+$ cd weatherForecast/<br>
+$ curl wttr.in/wohan</code>
+<br>
+</p><br>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/01.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/111978647-7096e780-8b04-11eb-9558-6ed12df9f6df.jpg" alt="" /></a>
+<h2>FORTUNE QUOTES
+<br><br></h2>
+<p>FORTUNE QUOTES (random quotes) o frases aleatorias para banners Termux.
+<br><br>
+<code>$ mkdir fortune<br>
+$ cd fortune<br>
+$ pkg install fortune -y<br>
+$ fortune
+</code><br><br>
+</p>
+<br><br>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/02.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/111868500-87113780-897a-11eb-832b-66adbd5e56d5.jpeg" alt="" /></a>
+<h2>FIGLET</h2>
+<p>FIGLET: Escribe ASCII en la Terminal de Termux..
+<br>
+<code>$ pkg install figlet<br>
+$ figlet<br>
+//(Y escribir..)*
+</code>
+<br>
+<br>
+<br></p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/03.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/111780679-02f67b80-88b8-11eb-93c5-955b3bd34151.jpeg" alt="" /></a>
+<h2>CMATRIX</h2>
+<p>CMATRIX:
+<br>Screensaver de MATRIX para Termux.
+<br><br><code>$ apt-get install cmatrix && cmatrix
+</code>
+<br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/04.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/111638836-01b34900-87fb-11eb-9ed6-30ee2f7a6830.jpeg" alt="" /></a>
+<h2>CACAFIRE</h2>
+<p>CACAFIRE: ScreenSaver on Fire.
+<br>
+<code>$ mkdir cacafire<br>
+$ cd cacafire<br>
+$ pkg install libcaca<br>
+$ cacafire
+</code>
+<br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/05.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112434252-a544b200-8d43-11eb-9102-8e62cc6ef5de.jpg" alt="" /></a>
+<h2>TECLADO LARGO</h2>
+<br>
+<p>
+Instala en Android teclas extra.
+<br>
+<code>
+$ wget https://raw.githubusercontent.com/barba99/TERMUX-TECLADO/master/TERMUX-TECLADO && chmod +x TERMUX-TECLADO && ./TERMUX-TECLADO && rm -irf TERMUX-TECLADO
+</code>
+<br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/06.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112637968-07cea880-8e3f-11eb-9380-e298e09f692e.jpg" alt="" /></a>
+<h2>Ahorcado</h2>
+<p><br>Juego del Ahorcado..<br><br>
+<code>$ wget https://raw.githubusercontent.com/barba99/Games/master/ahorcado.py <br>
+$ python3 ahorcado.py
+</code>
+<br><br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/07.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112721999-534c8980-8f07-11eb-88ed-8c17c18630cf.jpg" alt="" /></a>
+<h2>Efemérides</h2>
+<p>Calendario de Efemérides (por paises) en TERMUX..
+<br><br>
+<code>$ pkg install gcal<br>
+$ gcal -qes -cey
+</code>
+<br><br><br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/08.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112750374-899e0d80-8fc8-11eb-875d-250893467672.jpg" alt="" /></a>
+<h2>MAPSCII</h2>
+<p>
+MAPSCII, pequeño mapa ASCII para TERMUX.. <i>(A y Z = zoom)</i>.
+<br>
+<code>$ telnet mapscii.me
+</code><br>
+<br><br>
+</p><br><br>
+</article>
+<!-- //ARTICULO -->
+
+<!-- ARTICULO -->
+<article class="thumb">
+<a href="images/fulls/09.jpg" class="image"><img src="https://user-images.githubusercontent.com/80227002/112824485-dea25800-908a-11eb-8455-f7a8eeb43f53.jpg" alt="" /></a>
+<h2>COWSAY</h2>
+<p>COWSAY: Saca el texto en la Bash..
+<br><br>
+<code>$ mkdir cowsay<br>
+$ cd cowsay/     <br>                       
+$ pkg install cowsay<br>
+$ cowsay TEXTO
+</code>
+<br><br><br>
+</p>
+</article>
+<!-- //ARTICULO -->
+
+
+
+</div>
+<!-- -------------------------//ARTICULOS: -->
+
+<!-- --------------------------------------------------------------------Footer 
+					<footer id="footer" class="panel">
+						<div class="inner split">
+							<div>
+
+								
+								
+							</div>
+						</div>
+					</footer>-->
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
